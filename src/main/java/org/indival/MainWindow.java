579c40049ca1e006dabb06f44bed59421cac2ca3
devste <stephen+github@mailbox.org>
2017-04-26 08:19:20 +0200

    Clean-up of menu

--- src/main/java/org/indival/MainWindow.java
+++ src/main/java/org/indival/MainWindow.java
@@ -43,237 +43,244 @@ import com.mxgraph.swing.mxGraphComponent;
 
 public class MainWindow extends JFrame implements ActionListener {
 
-    private static final long serialVersionUID = -8706457057984469938L;
-    private ResourceBundle messages;
-    private final JFileChooser fc = new JFileChooser();
-    private Project project;
-    private String projectFile;
-    private JScrollPane graphPane;
-    private JLabel startLabel;
-    private MxGraphEdit mge;
-    private DecisionOptionControl doc;
-    private transient Logger log = LoggerFactory.getLogger(this.getClass().toString());
+	private static final long serialVersionUID = -8706457057984469938L;
+	private ResourceBundle messages;
+	private final JFileChooser fc = new JFileChooser();
+	private Project project;
+	private String projectFile;
+	private JScrollPane graphPane;
+	private JLabel startLabel;
+	private MxGraphEdit mge;
+	private DecisionOptionControl doc;
+	private transient Logger log = LoggerFactory.getLogger(this.getClass().toString());
 
-    public MainWindow() {
-	super("INfluence DIagramme with VALues");
+	public MainWindow() {
+		super("INfluence DIagramme with VALues");
 
-	log.info("Default character encoding: " + Charset.defaultCharset().name());
+		log.info("Default character encoding: " + Charset.defaultCharset().name());
 
-	Locale currentLocale = Locale.getDefault();
-	messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
+		Locale currentLocale = Locale.getDefault();
+		messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
 
-	try {
-	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-		| UnsupportedLookAndFeelException e) {
-	    log.debug(e.getMessage());
-	}
-
-	setBackground(Color.WHITE);
-
-	setJMenuBar(menuBar());
-	showStartMessage();
-
-	// Display the window.
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	initEmptyProject();
-	pack();
-	setVisible(true);
-
-    }
-
-    private JMenuBar menuBar() {
-	JMenuBar theMenuBar = new JMenuBar();
-	JMenu menu;
-	JMenuItem menuItem;
-
-	// Layout of the menu bar
-	theMenuBar.setOpaque(true);
-	theMenuBar.setPreferredSize(new Dimension(600, 20));
-
-	// Build the first level of the menu.
-	menu = new JMenu(messages.getString("menu.project"));
-	menu.setMnemonic(KeyEvent.VK_P);
-	menu.getAccessibleContext().setAccessibleDescription(messages.getString("menu.project.desc"));
-
-	menuItem = new JMenuItem(messages.getString("menu.project.open"));
-	menuItem.setMnemonic(KeyEvent.VK_O);
-	menuItem.setActionCommand("projectOpen");
-	menuItem.addActionListener(this);
-	menu.add(menuItem);
-
-	menu.addActionListener(this);
-
-	theMenuBar.add(menu);
-
-	menu = new JMenu(messages.getString("menu.view"));
-	menu.setMnemonic(KeyEvent.VK_V);
-	menu.getAccessibleContext().setAccessibleDescription(messages.getString("menu.view.desc"));
-
-	menuItem = new JMenuItem(messages.getString("menu.view.id"));
-	menuItem.setMnemonic(KeyEvent.VK_I);
-	menuItem.setActionCommand("showIDEdit");
-	menuItem.addActionListener(this);
-	menu.add(menuItem);
-
-	menuItem = new JMenuItem(messages.getString("menu.view.dg"));
-	menuItem.setMnemonic(KeyEvent.VK_D);
-	menuItem.setActionCommand("showDGEdit");
-	menuItem.addActionListener(this);
-	menu.add(menuItem);
-
-	menuItem = new JMenuItem(messages.getString("menu.view.layout"));
-	menuItem.setMnemonic(KeyEvent.VK_L);
-	menuItem.setActionCommand("applyLayout");
-	menuItem.addActionListener(this);
-	menu.add(menuItem);
-
-	menu.addActionListener(this);
-
-	theMenuBar.add(menu);
-
-	menu = new JMenu(messages.getString("menu.example"));
-	menu.setMnemonic(KeyEvent.VK_E);
-
-	menuItem = new JMenuItem(messages.getString("menu.example.load"));
-	menuItem.setMnemonic(KeyEvent.VK_L);
-	menuItem.setActionCommand("projectExample");
-	menuItem.addActionListener(this);
-	menu.add(menuItem);
-
-	theMenuBar.add(menu);
-
-	return theMenuBar;
-
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-	JScrollPane cp = this.graphPane;
-	switch (e.getActionCommand()) {
-	case "showIDEdit":
-	    showIDEdit();
-	    break;
-	case "applyLayout":
-	    applyLayout();
-	    break;
-	case "projectOpen":
-	    openProjectFile(cp);
-	    break;
-	case "projectExample":
-	    openExampleProjectFile();
-	    break;
-	case "updateID":
-	    updateID();
-	    break;
-	default:
-	    break;
-	}
-    }
-
-    private void openProjectFile(Container cp) {
-	int returnOpen = fc.showOpenDialog(cp);
-	if (returnOpen == JFileChooser.APPROVE_OPTION) {
-	    File ofile = fc.getSelectedFile();
-	    if (ofile.exists()) {
 		try {
-		    this.projectFile = ofile.getCanonicalPath();
-		    InputStream fis = new FileInputStream(this.projectFile);
-		    processFile(fis);
-		    showIDEdit();
-		} catch (IOException e) {
-		    log.debug(e.getMessage());
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			log.debug(e.getMessage());
 		}
-	    }
+
+		setBackground(Color.WHITE);
+
+		setJMenuBar(menuBar());
+		showStartMessage();
+
+		// Display the window.
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initEmptyProject();
+		pack();
+		setVisible(true);
+
 	}
-    }
 
-    private void openExampleProjectFile() {
-	String filename = "testfile.txt";
-	log.info("reading example file " + filename);
-	InputStream in = getClass().getResourceAsStream("/" + filename);
-	processFile(in);
-	showIDEdit();
-    }
+	private JMenuBar menuBar() {
+		JMenuBar theMenuBar = new JMenuBar();
+		JMenu menu;
+		JMenuItem menuItem;
 
-    private void showStartMessage() {
-	startLabel = new JLabel(messages.getString("startScreenMessage"));
-	startLabel.setBackground(Color.DARK_GRAY);
-	startLabel.setForeground(Color.white);
-	startLabel.setOpaque(true);
-	startLabel.setPreferredSize(new Dimension(400, 200));
-	startLabel.setHorizontalAlignment(SwingConstants.CENTER);
-	this.getContentPane().add(startLabel);
-    }
+		// Layout of the menu bar
+		theMenuBar.setOpaque(true);
+		theMenuBar.setPreferredSize(new Dimension(600, 20));
 
-    private void removeStartMessage() {
-	this.getContentPane().remove(startLabel);
-    }
+		// Build the first level of the menu.
+		menu = new JMenu(messages.getString("menu.project"));
+		menu.setMnemonic(KeyEvent.VK_P);
+		menu.getAccessibleContext().setAccessibleDescription(messages.getString("menu.project.desc"));
 
-    private void processFile(InputStream fis) {
-	indigraphParser idg;
-	try {
-	    InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
-	    idg = IdgParser.parse(isr);
-	    indigraphParser.IndigraphContext idgctx = idg.indigraph();
-	    IdgFileVisitor visitor = new IdgFileVisitor(project);
-	    visitor.visit(idgctx);
-	} catch (IOException e) {
-	    log.debug(e.getMessage());
+		menuItem = new JMenuItem(messages.getString("menu.project.open"));
+		menuItem.setMnemonic(KeyEvent.VK_O);
+		menuItem.setActionCommand("projectOpen");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+
+		menuItem = new JMenuItem(messages.getString("menu.project.example"));
+		menuItem.setMnemonic(KeyEvent.VK_L);
+		menuItem.setActionCommand("projectExample");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+
+		menuItem = new JMenuItem(messages.getString("menu.project.exit"));
+		menuItem.setMnemonic(KeyEvent.VK_E);
+		menuItem.setActionCommand("exit");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+
+		menu.addActionListener(this);
+
+		theMenuBar.add(menu);
+
+		menu = new JMenu(messages.getString("menu.view"));
+		menu.setMnemonic(KeyEvent.VK_V);
+		menu.getAccessibleContext().setAccessibleDescription(messages.getString("menu.view.desc"));
+
+		menuItem = new JMenuItem(messages.getString("menu.view.layout"));
+		menuItem.setMnemonic(KeyEvent.VK_L);
+		menuItem.setActionCommand("applyLayout");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+
+		menu.addActionListener(this);
+
+		theMenuBar.add(menu);
+
+		menu = new JMenu(messages.getString("menu.about"));
+		menu.setMnemonic(KeyEvent.VK_A);
+
+		menuItem = new JMenuItem(messages.getString("menu.about.info"));
+		menuItem.setMnemonic(KeyEvent.VK_I);
+		menuItem.setActionCommand("infoBox");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+
+		theMenuBar.add(menu);
+
+		return theMenuBar;
+
 	}
-    }
 
-    private void applyLayout() {
-	this.mge.layoutCompactTree();
-	Container cp = getContentPane();
-	cp.revalidate();
-	cp.repaint();
-	pack();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JScrollPane cp = this.graphPane;
+		switch (e.getActionCommand()) {
+		case "showIDEdit":
+			showIDEdit();
+			break;
+		case "applyLayout":
+			applyLayout();
+			break;
+		case "projectOpen":
+			openProjectFile(cp);
+			break;
+		case "projectExample":
+			openExampleProjectFile();
+			break;
+		case "updateID":
+			updateID();
+			break;
+		case "exit":
+			exit();
+			break;
+		default:
+			break;
+		}
+	}
 
-    private void showIDEdit() {
-	removeStartMessage();
-	this.mge = new MxGraphEdit(this.project.getModel(), this.messages);
-	mxGraphComponent comp = this.mge.getComponent();
-	comp.getViewport().setOpaque(true);
-	comp.getViewport().setBackground(Color.WHITE);
-	Container cp = this.getContentPane();
-	cp.add(this.mge.getComponent(), BorderLayout.CENTER);
-	cp.revalidate();
-	cp.repaint();
-	pack();
+	private void openProjectFile(Container cp) {
+		int returnOpen = fc.showOpenDialog(cp);
+		if (returnOpen == JFileChooser.APPROVE_OPTION) {
+			File ofile = fc.getSelectedFile();
+			if (ofile.exists()) {
+				try {
+					this.projectFile = ofile.getCanonicalPath();
+					InputStream fis = new FileInputStream(this.projectFile);
+					processFile(fis);
+					showIDEdit();
+				} catch (IOException e) {
+					log.debug(e.getMessage());
+				}
+			}
+		}
+	}
 
-	// Decision option control
-	this.doc = new DecisionOptionControl(project.getSelectionNodeList(), this);
-	cp.add(this.doc, BorderLayout.LINE_START);
-	cp.revalidate();
-	cp.repaint();
-	pack();
-    }
+	private void openExampleProjectFile() {
+		String filename = "testfile.txt";
+		log.info("reading example file " + filename);
+		InputStream in = getClass().getResourceAsStream("/" + filename);
+		processFile(in);
+		showIDEdit();
+	}
 
-    /*
-     * Updates the influence diagramme. Should usually be triggered by events.
-     */
-    public void updateID() {
-	log.info("updating Influence diagramme");
-	Container cp = this.getContentPane();
-	this.mge.getComponent().refresh();
-	cp.revalidate();
-	cp.repaint();
-	this.mge.getComponent().setVisible(true);
-	cp.revalidate();
-	cp.repaint();
-	pack();
-    }
+	private void exit() {
+		System.exit(0);
+	}
 
-    private void initEmptyProject() {
-	project = new Project();
-    }
+	private void showStartMessage() {
+		startLabel = new JLabel(messages.getString("startScreenMessage"));
+		startLabel.setBackground(Color.DARK_GRAY);
+		startLabel.setForeground(Color.white);
+		startLabel.setOpaque(true);
+		startLabel.setPreferredSize(new Dimension(400, 200));
+		startLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		this.getContentPane().add(startLabel);
+	}
+
+	private void removeStartMessage() {
+		this.getContentPane().remove(startLabel);
+	}
+
+	private void processFile(InputStream fis) {
+		indigraphParser idg;
+		try {
+			InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+			idg = IdgParser.parse(isr);
+			indigraphParser.IndigraphContext idgctx = idg.indigraph();
+			IdgFileVisitor visitor = new IdgFileVisitor(project);
+			visitor.visit(idgctx);
+		} catch (IOException e) {
+			log.debug(e.getMessage());
+		}
+	}
+
+	private void applyLayout() {
+		this.mge.layoutCompactTree();
+		Container cp = getContentPane();
+		cp.revalidate();
+		cp.repaint();
+		pack();
+	}
+
+	private void showIDEdit() {
+		removeStartMessage();
+		this.mge = new MxGraphEdit(this.project.getModel(), this.messages);
+		mxGraphComponent comp = this.mge.getComponent();
+		comp.getViewport().setOpaque(true);
+		comp.getViewport().setBackground(Color.WHITE);
+		Container cp = this.getContentPane();
+		cp.add(this.mge.getComponent(), BorderLayout.CENTER);
+		cp.revalidate();
+		cp.repaint();
+		pack();
+
+		// Decision option control
+		this.doc = new DecisionOptionControl(project.getSelectionNodeList(), this);
+		cp.add(this.doc, BorderLayout.LINE_START);
+		cp.revalidate();
+		cp.repaint();
+		pack();
+	}
+
+	/*
+	 * Updates the influence diagramme. Should usually be triggered by events.
+	 */
+	public void updateID() {
+		log.info("updating Influence diagramme");
+		Container cp = this.getContentPane();
+		this.mge.getComponent().refresh();
+		cp.revalidate();
+		cp.repaint();
+		this.mge.getComponent().setVisible(true);
+		cp.revalidate();
+		cp.repaint();
+		pack();
+	}
+
+	private void initEmptyProject() {
+		project = new Project();
+	}
 
 }
